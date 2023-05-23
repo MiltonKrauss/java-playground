b3100a2503a022e164c2a662b2270596a3928ca2
Milton Krauss <miltonkrauss@gmail.com>
2023-05-23 15:36:16 -0400

    completed MakeLast method

--- DaysOfCode/src/main/java/org/example/Main.java
+++ DaysOfCode/src/main/java/org/example/Main.java
@@ -3,8 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
         int[] intArrayForMakeMiddle = {1, 2, 3, 4};
         int[] makeMiddleResult = MakeMiddle.makeMiddle(intArrayForMakeMiddle);
         System.out.println(java.util.Arrays.toString(makeMiddleResult));
+
+        int[] intArrayForMakeLast = {1, 2, 3};
+        int[] makeLastResult = MakeLast.makeLast(intArrayForMakeLast);
+        System.out.println(java.util.Arrays.toString(makeLastResult));
+
+
     }
 }
